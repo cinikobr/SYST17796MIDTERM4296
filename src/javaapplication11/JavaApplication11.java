f998d6322511b7c1cb956d9a86019ce1c3266b69
cinikobr <cinikobr@sheridancollege.ca>
2022-02-24 13:11:33 -0500

    Modified functionality to accept and validate user input.

--- src/javaapplication11/JavaApplication11.java
+++ src/javaapplication11/JavaApplication11.java
@@ -5,33 +5,34 @@
  */
 package javaapplication11;
 
+import java.util.Scanner;
+
 /**
  *
  * @author r2kar
  */
-
 public class JavaApplication11 {
 
     /**
      * @param args the command line arguments
      */
     Seasons season;
-    private boolean abc=true;
-    public enum Seasons{
+    private boolean abc = true;
+
+    public enum Seasons {
         FALL,
         WINTER,
         SPRING,
         SUMMER
     }
+
     //a constructor
-    public JavaApplication11(Seasons season)
-    {
+    public JavaApplication11(Seasons season) {
         this.season = season;
     }
-   
-    public void seasonDescription()
-    {
-       switch (season){
+
+    public void seasonDescription() {
+        switch (season) {
             case FALL:
                 System.out.println("my fav. season!");
                 break;
@@ -44,25 +45,42 @@ public class JavaApplication11 {
             case SUMMER:
                 System.out.println("it is hot!");
                 break;
-     }
+        }
     }
+
     public static void main(String[] args) {
         // TODO code application logic here
         // try to get the input (myStr) from the user
-        String myStr= "SUMMER";
-        JavaApplication11 test1= new JavaApplication11(Seasons.valueOf(myStr));
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Please enter season: ");
+        String myStr = scanner.nextLine();
+
+        //Keeps asking for a season until a valid one is given
+        while (!isSeason(myStr)) {
+            System.out.println("Not a valid Season: ");
+            System.out.print("Please enter season: ");
+            myStr = scanner.nextLine();
+        }
+
+        JavaApplication11 test1 = new JavaApplication11(Seasons.valueOf(myStr));
         test1.seasonDescription();
         //test1.isSeason();
         int ord = Seasons.valueOf(myStr).ordinal();
         System.out.println(Seasons.valueOf(myStr).ordinal());
         //a for loop that iterated thru the Seasons Enum using values() method
-        for (Seasons mySeason: Seasons.values())
-        {
-            
+        for (Seasons mySeason : Seasons.values()) {
+
             System.out.println(mySeason);
         }
-       
-        }
-    }
-    
 
+    }
+
+    private static boolean isSeason(String text) {
+        for (Seasons season : Seasons.values()) {
+            if (season.name().equals(text)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
